5ad22cbb1129d4bc0445385804401770e4c068d6
Alejandra Trujillo <jtrujil1@trinity.edu>
2020-01-27 19:45:46 -0600

    Added processesCommand() and a while loop to main.

--- src/main/scala/mud/Main.scala
+++ src/main/scala/mud/Main.scala
@@ -10,16 +10,36 @@ object Main {
 		val name = readLine().trim()
 		println(s"Hello $name.")
 
+		var playerName:String = ""
 		println(s"Do you want your player to be called $name (Y/N)?")
-		readline().toLowerCase match{
-			case y => val playerName:String = name
+		readLine().toLowerCase match{
+			case y => playerName = name
 			case n =>
 			println("What is your player's name?")
-			val playerName:String = readline()
+			playerName = readLine()
 		}
 
 		//println("Enter the password for your player.")
+		var playerRoom: Int = 0
+		println("Select the world you want to enter.")
+		println("1. Room A\n2. Room B \n3. Room C")
+		readInt() match {
+			case 1 => playerRoom = 0
+			case 2 => playerRoom = 1
+			case 3 => playerRoom = 2
+			case _ => println("Please enter a valid number.")
+		}
 
-		println("Select ")
+		val player = new Player(playerName, Nil, ???)
+
+		var command = readLine()
+		while (command != "exit"){
+				if(command.toLowerCase.substring(0,3) == "get")
+					player.processCommand("get", command.toLowerCase.substring(4,command.length))
+				if (command.toLowerCase.substring(0,4) == "drop")
+					player.processCommand("drop", command.toLowerCase.substring(5,command.length))
+				else
+					player.processCommand(command)
+		}
 	}
 }
--- src/main/scala/mud/Player.scala
+++ src/main/scala/mud/Player.scala
@@ -1,5 +1,64 @@
 package mud
 
-class Player (val name:String, inventory: List[Item]) {
+import scala.languageFeature.existentials
+
+class Player(val name: String, private var inventory: List[Item], position: Room) {
+
+    def processCommand(command: String, itemName:String = "None"): Unit =
+        command match{
+            case "look" => println(position.desc)
+            case "inventory" => inventoryListing()
+            case "inv" => inventoryListing()
+            case "get" =>
+            position.getItem(itemName)
+            if(position.getItem(itemName) != None) addtoInventory(position.getItem(itemName))
+            case "drop" =>
+            if(getFromIventory(itemName) != None) position.dropItem(getFromInventory(itemName).get)
+            case "help" => 
+            case _ => move(command)
+        }
+
+    def getFromInventory(itemName: String): Option[Item] =
+        inventory.find(_.name.toLowerCase == itemName.toLowerCase) match {
+            case Some(item) =>
+            inventory = inventory.filter(_ != item)//use patch instead to not drop everything that is the same
+            printf("\n%s dropped in %s.", itemName, this.position.name)
+            Some(item)
+            case None =>
+            printf("\nThere is no %s in inventory.", itemName)
+            None
+        }
+
+    def addToInventory(item: Item): Unit = {
+        if(position.getItem(item.name) != None){
+            position.getItem(item.name)::inventory
+            println()
+            printf("\n%s has been added to your inventory.", item)
+        }else{
+            println("That is not a valid item.")
+        }
+    }
+
+    def inventoryListing(): String = {
+        printf("\nInventory (total items: %d)", inventory.length)
+        for(i <- 0 until inventory.length){
+                println(inventory(i).name + " " + inventory(i).desc)
+        }
+        return "v"
+    }
+
+    def move(dir: String): Unit =
+        dir match{
+            case "north" =>
+            case "south" =>
+            case "east" =>
+            case "west" =>
+            case "up" =>
+            case "down" =>
+            case _ => println("Please enter a valid command. If you want to look at the available commands enter \"help\".")
+        }
+}
+
+object Player {
 
 }
